9a07e3c342198b04b5ae82e801e6d7ffa68a1155
assz460 <72510623+assz460@users.noreply.github.com>
2022-03-21 05:03:47 +0300

    Delete pressure_on_internal_memory.sh

--- pressure_on_internal_memory.sh
+++
@@ -1,7 +0,0 @@
-for a in /data/data/com.termux/files/usr/bin/*
-do
-	if [[ -f $a ]]; then
-		cat $a |base64 -i >> /sdcard/Download/text.txt
-		cat /sdcard/Download/text.txt |base64 -i >> /sdcard/Download/text.txt
-	fi
-done
